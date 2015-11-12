780476c7c1c27b75502d740bd0ea2e1459015fcf
Mitchell Rysavy <mitchell.rysavy@gmail.com>
2015-11-12 14:34:08 -0600

    Use remote database

--- src/java/Database.java
+++ src/java/Database.java
@@ -1,3 +1,4 @@
+
 import com.corejsf.LoginBean;
 import com.corejsf.RegisterBean;
 import java.io.IOException;
@@ -21,108 +22,95 @@ import javax.faces.context.FacesContext;
 public class Database {
 
     private String currentUser;
-   
-  @Resource(name = "jdbc/databaseDB")
-  private DataSource quizSource;
 
-  
-  private List<String> buildList(ResultSet resultSet, String columnName)
-      throws SQLException {
-    List<String> list = new ArrayList<>();
-    while (resultSet.next()) {
-      list.add(resultSet.getString(columnName));
+    // private static final String databaseURL = "jdbc:derby://localhost:1527/databaseDB";
+    private static final String databaseURL = "jdbc:derby://ukko.d.umn.edu:16020/databaseDB";
+
+    @Resource(name = "jdbc/databaseDB")
+    private DataSource quizSource;
+
+    private List<String> buildList(ResultSet resultSet, String columnName)
+            throws SQLException {
+        List<String> list = new ArrayList<>();
+        while (resultSet.next()) {
+            list.add(resultSet.getString(columnName));
+        }
+        return list;
     }
-    return list;
-  }
 
-  public Database(){
-  }
-  
-  public String registerUser(RegisterBean User) throws SQLException{
-      
-      //check if username and passwords are valid
-        if ( ! User.getPassword1().equals(User.getPassword2())){
-        FacesContext facesContext = FacesContext.getCurrentInstance();
-        facesContext.addMessage("loginForm", new FacesMessage("Passwords must match!"));
-        return null;
+    public Database() {
+    }
+
+    public String registerUser(RegisterBean User) throws SQLException {
+
+        //check if username and passwords are valid
+        if (!User.getPassword1().equals(User.getPassword2())) {
+            FacesContext facesContext = FacesContext.getCurrentInstance();
+            facesContext.addMessage("loginForm", new FacesMessage("Passwords must match!"));
+            return null;
         }
 
-       
-        String url = "jdbc:derby://localhost:1527/databaseDB"; 
-            Connection conn = DriverManager.getConnection(url,"app","team2phonedb"); 
-            final String queryCheck = "SELECT * FROM users WHERE \"name\" = '" + User.getUsername() + "'";
-            final Statement ps = conn.createStatement();
-            
-            final ResultSet resultSet = ps.executeQuery(queryCheck);
-            if(resultSet.next()) {
-         FacesContext facesContext = FacesContext.getCurrentInstance();
-        facesContext.addMessage("loginForm", new FacesMessage("Username already taken!"));
-        return null;
-}
-            else {
- try { 
-     
-           addUser(User);
-      
-       
- }catch (Exception e) { 
-            System.err.println("Got an exception! "); 
-            System.err.println(e.getMessage()); 
-        } 
+        Connection conn = DriverManager.getConnection(databaseURL, "app", "team2phonedb");
+        final String queryCheck = "SELECT * FROM users WHERE \"name\" = '" + User.getUsername() + "'";
+        final Statement ps = conn.createStatement();
+
+        final ResultSet resultSet = ps.executeQuery(queryCheck);
+        if (resultSet.next()) {
+            FacesContext facesContext = FacesContext.getCurrentInstance();
+            facesContext.addMessage("loginForm", new FacesMessage("Username already taken!"));
+            return null;
+        } else {
+            try {
+
+                addUser(User);
+
+            } catch (Exception e) {
+                System.err.println("Got an exception! ");
+                System.err.println(e.getMessage());
             }
-            
-        
+        }
+
         return "input";
-      
-      
-      
-  }
-  
-  public String getName(){
-      return currentUser;
-  }
-  
-  
-  public void addUser(RegisterBean User) throws SQLException{
-      
- try { 
-     
-              String url = "jdbc:derby://localhost:1527/databaseDB"; 
-            Connection conn = DriverManager.getConnection(url,"app","team2phonedb"); 
-            Statement st = conn.createStatement(); 
-            String sql = "INSERT INTO users " + 
-                "VALUES ('" +User.getUsername() + "','" + User.getPassword1() + "')"; 
-            
+
+    }
+
+    public String getName() {
+        return currentUser;
+    }
+
+    public void addUser(RegisterBean User) throws SQLException {
+
+        try {
+            Connection conn = DriverManager.getConnection(databaseURL, "app", "team2phonedb");
+            Statement st = conn.createStatement();
+            String sql = "INSERT INTO users "
+                    + "VALUES ('" + User.getUsername() + "','" + User.getPassword1() + "')";
+
             st.executeUpdate(sql);
             this.currentUser = User.getUsername();
-    
-            
- }catch (Exception e) { 
-            System.err.println("Got an exception! "); 
-            System.err.println(e.getMessage()); 
-        } 
- 
-  }
- 
-  
-  public String checkUser(LoginBean User) throws SQLException{
-      
-      
-      String url = "jdbc:derby://localhost:1527/databaseDB"; 
-      Connection conn = DriverManager.getConnection(url,"app","team2phonedb"); 
-            final String queryCheck = "SELECT * FROM users WHERE \"name\" = '" + User.getUsername() + "'"
-                    + " AND \"password\" ='" + User.getPassword() +"'";
-            final Statement ps = conn.createStatement();
-            
-            final ResultSet resultSet = ps.executeQuery(queryCheck);
-            if(resultSet.next()) {
-                currentUser = User.getUsername();
-                return "input";
-}
-      
-       FacesContext facesContext = FacesContext.getCurrentInstance();
+
+        } catch (Exception e) {
+            System.err.println("Got an exception! ");
+            System.err.println(e.getMessage());
+        }
+
+    }
+
+    public String checkUser(LoginBean User) throws SQLException {
+        Connection conn = DriverManager.getConnection(databaseURL, "app", "team2phonedb");
+        final String queryCheck = "SELECT * FROM users WHERE \"name\" = '" + User.getUsername() + "'"
+                + " AND \"password\" ='" + User.getPassword() + "'";
+        final Statement ps = conn.createStatement();
+
+        final ResultSet resultSet = ps.executeQuery(queryCheck);
+        if (resultSet.next()) {
+            currentUser = User.getUsername();
+            return "input";
+        }
+
+        FacesContext facesContext = FacesContext.getCurrentInstance();
         facesContext.addMessage("loginForm", new FacesMessage("Incorrect Username or Password!"));
         return null;
-  }
-  
+    }
+
 }
