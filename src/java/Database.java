edbb44b47b63ab190e98dfacf3cfc73d5d7583c9
Mitchell Rysavy <mitchell.rysavy@gmail.com>
2015-11-30 23:50:26 -0600

    Use database resource properly

--- src/java/Database.java
+++ src/java/Database.java
@@ -23,10 +23,8 @@ public class Database {
 
     private String currentUser;
 
-    // private static final String databaseURL = "jdbc:derby://localhost:1527/databaseDB";
-    private static final String databaseURL = "jdbc:derby://ukko.d.umn.edu:16020/databaseDB";
-
     @Resource(name = "jdbc/databaseDB")
+    private DataSource dataSource;
 
     private List<String> buildList(ResultSet resultSet, String columnName)
             throws SQLException {
@@ -45,65 +43,62 @@ public class Database {
         //boolean hasSpecialChars = false;
         boolean hasNums = false;
         boolean hasLetters = false;
-        
+
         //check if username and passwords are valid
         if (!User.getPassword1().equals(User.getPassword2())) {
             FacesContext facesContext = FacesContext.getCurrentInstance();
             facesContext.addMessage("loginForm", new FacesMessage("Passwords must match!"));
             return null;
-        }
-        
-        // check if password is long enough
+        } // check if password is long enough
         else if (User.getPassword1().length() < 8) {
             FacesContext facesContext = FacesContext.getCurrentInstance();
             facesContext.addMessage("loginForm", new FacesMessage("Passwords must be at least 8 characters long!"));
             return null;
-        }
-        
-        else if (User.getPassword1().length() > 16) {
+        } else if (User.getPassword1().length() > 16) {
             FacesContext facesContext = FacesContext.getCurrentInstance();
             facesContext.addMessage("loginForm", new FacesMessage("Passwords must be less than 16 characters long!"));
             return null;
         }
-        
-        for (int i = 0; i< User.getPassword1().length(); i++){
-            if (Character.isDigit((User.getPassword1().charAt(i)))){
+
+        for (int i = 0; i < User.getPassword1().length(); i++) {
+            if (Character.isDigit((User.getPassword1().charAt(i)))) {
                 hasNums = true;
             }
-            if ( Character.isLetter(User.getPassword1().charAt(i))){
+            if (Character.isLetter(User.getPassword1().charAt(i))) {
                 hasLetters = true;
             }
         }
-        
-       if (! hasLetters){
+
+        if (!hasLetters) {
             FacesContext facesContext = FacesContext.getCurrentInstance();
             facesContext.addMessage("loginForm", new FacesMessage("Passwords must contain at least one letter!"));
             return null;
         }
-        
-        if (! hasNums){
+
+        if (!hasNums) {
             FacesContext facesContext = FacesContext.getCurrentInstance();
             facesContext.addMessage("loginForm", new FacesMessage("Passwords must contain at least one number!"));
             return null;
         }
-        
-        Connection conn = DriverManager.getConnection(databaseURL, "app", "team2phonedb");
-        final String queryCheck = "SELECT * FROM users WHERE \"name\" = '" + User.getUsername() + "'";
-        final Statement ps = conn.createStatement();
 
-        final ResultSet resultSet = ps.executeQuery(queryCheck);
-        if (resultSet.next()) {
-            FacesContext facesContext = FacesContext.getCurrentInstance();
-            facesContext.addMessage("loginForm", new FacesMessage("Username already taken!"));
-            return null;
-        } else {
-            try {
+        try (Connection conn = dataSource.getConnection()) {
+            final String queryCheck = "SELECT * FROM users WHERE \"name\" = '" + User.getUsername() + "'";
+            final Statement ps = conn.createStatement();
 
-                addUser(User);
+            final ResultSet resultSet = ps.executeQuery(queryCheck);
+            if (resultSet.next()) {
+                FacesContext facesContext = FacesContext.getCurrentInstance();
+                facesContext.addMessage("loginForm", new FacesMessage("Username already taken!"));
+                return null;
+            } else {
+                try {
 
-            } catch (Exception e) {
-                System.err.println("Got an exception! ");
-                System.err.println(e.getMessage());
+                    addUser(User);
+
+                } catch (Exception e) {
+                    System.err.println("Got an exception! ");
+                    System.err.println(e.getMessage());
+                }
             }
         }
 
@@ -117,8 +112,7 @@ public class Database {
 
     public void addUser(RegisterBean User) throws SQLException {
 
-        try {
-            Connection conn = DriverManager.getConnection(databaseURL, "app", "team2phonedb");
+        try (Connection conn = dataSource.getConnection()) {
             Statement st = conn.createStatement();
             String sql = "INSERT INTO users "
                     + "VALUES ('" + User.getUsername() + "','" + User.getPassword1() + "')";
@@ -134,17 +128,17 @@ public class Database {
     }
 
     public String checkUser(LoginBean User) throws SQLException {
-        Connection conn = DriverManager.getConnection(databaseURL, "app", "team2phonedb");
-        final String queryCheck = "SELECT * FROM users WHERE \"name\" = '" + User.getUsername() + "'"
-                + " AND \"password\" ='" + User.getPassword() + "'";
-        final Statement ps = conn.createStatement();
+        try (Connection conn = dataSource.getConnection()) {
+            final String queryCheck = "SELECT * FROM users WHERE \"name\" = '" + User.getUsername() + "'"
+                    + " AND \"password\" ='" + User.getPassword() + "'";
+            final Statement ps = conn.createStatement();
 
-        final ResultSet resultSet = ps.executeQuery(queryCheck);
-        if (resultSet.next()) {
-            currentUser = User.getUsername();
-            return "input";
+            final ResultSet resultSet = ps.executeQuery(queryCheck);
+            if (resultSet.next()) {
+                currentUser = User.getUsername();
+                return "input";
+            }
         }
-
         FacesContext facesContext = FacesContext.getCurrentInstance();
         facesContext.addMessage("loginForm", new FacesMessage("Incorrect Username or Password!"));
         return null;
