c72eee6472176ea6505f5a001d49ffca2ed52314
Caleb Reder <rede0060@d.umn.edu>
2015-12-01 13:54:48 -0600

    Added more plans to DB

--- web/WEB-INF/sql/plan_insert.sql
+++ web/WEB-INF/sql/plan_insert.sql
@@ -8,3 +8,11 @@ Insert into plans
     Values (Default, 'Sprint', 'Starter Unlimited Data', 'https://www.sprint.com/shop/plan-wall/#!/?plan=individual', 'TRUE', 'TRUE', 'TRUE', 'FALSE','FALSE', 0, 0, 1, 40);
 Insert into plans
     Values (Default, 'T-Mobile', 'Simple Choice Plan', 'http://www.t-mobile.com/cell-phone-plans.html', 'TRUE', 'TRUE', 'TRUE', 'FALSE','FALSE', 0, 0, 2, 50);
+Insert into plans
+    Values (Default, 'Verizon','Medium', 'http://www.verizonwireless.com/landingpages/verizon-plan/', 'TRUE', 'TRUE', 'FALSE', 'FALSE', 'FALSE', 0, 0, 3, 45);
+Insert into plans
+    Values (Default, 'Verizon','Large', 'http://www.verizonwireless.com/landingpages/verizon-plan/', 'TRUE', 'TRUE', 'FALSE', 'FALSE', 'FALSE', 0, 0, 6, 60);
+Insert into plans
+    Values (Default, 'Verizon','Extra Large', 'http://www.verizonwireless.com/landingpages/verizon-plan/', 'TRUE', 'TRUE', 'FALSE', 'FALSE', 'FALSE', 0, 0, 12, 80);
+Insert into plans
+    Values (Default, 'Verizon','Double Extra Large', 'http://www.verizonwireless.com/landingpages/verizon-plan/', 'TRUE', 'TRUE', 'FALSE', 'FALSE', 'FALSE', 0, 0, 18, 100);
